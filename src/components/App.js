017ff8d1864362984f86be92c2ff4c80b3af1b64
yuriipereverziev <yuripereverzev@gmail.com>
2023-02-01 12:14:38 +0200

    homework_1 fix

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div className="App">
-                <p className="description">Миру-мир, студентам - beer.</p>
+                <p className="description">Миру-мир, студентам - beer</p>
             </div>
         )
     }
